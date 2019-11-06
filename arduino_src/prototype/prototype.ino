00927d4d980cb87d62894d6e93fc93bfbe49da18
= <=>
2019-11-06 13:24:58 -0500

    Added pwm pin setup for motors

--- arduino_src/prototype/prototype.ino
+++ arduino_src/prototype/prototype.ino
@@ -4,12 +4,25 @@
 #include <utility/imumaths.h>
 #include <BasicLinearAlgebra.h>
 
+// Setup PWM pin numbers for motors
+#define dir_1 2
+#define pwm_1 3
+#define dir_2 5
+#define pwm_2 6
+
 // Declare IMU instance
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 void setup(void) 
 {
+  // Initialize PWM pins as outputs
+  pinMode(pwm_1,OUTPUT);
+  pinMode(dir_1,OUTPUT);
+  pinMode(pwm_2,OUTPUT);
+  pinMode(dir_2,OUTPUT);
+    
   Serial.begin(9600);
+  delay(100);
   Serial.println("Orientation Sensor Test"); Serial.println("");
   
   /* Initialise the sensor */
