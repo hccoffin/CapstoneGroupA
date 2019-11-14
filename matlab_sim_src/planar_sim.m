bb4b927201411100624e537a01038dd8f24d833e
Nikolai Flowers <nikolaif399@gmail.com>
2019-11-14 17:07:56 -0500

    Updates to planar sim

--- matlab_sim_src/planar_sim.m
+++ matlab_sim_src/planar_sim.m
@@ -8,7 +8,6 @@ l = 0.4;
 i_b = m_b*l^2/12;
 r = 0.06;
 
-
 q = [theta; phi];
 dq = [dtheta; dphi];
 
@@ -29,11 +28,11 @@ Tau = [torque;0];
 ddq = simplify(inv(M)*(Tau - C*dq - N));
 %simplify(subs(ddq, [theta, dtheta, phi, dphi], [0, 0, 0, 0]))
 
-g = matlabFunction(ddq, 'Vars', [phi dphi torque]);
+ddq_fn = matlabFunction(ddq, 'Vars', [phi dphi torque]);
 
 tspan = [0 5];
 q0 = [0 0 0.2 0]';
-[t, y] = ode45(@(t,y) sim_state_update(t,y,g), tspan, q0);
+[t, y] = ode45(@(t,y) sim_state_update(t,y,ddq_fn), tspan, q0);
 visualize(t,y,r,l)
 
 
--- matlab_sim_src/sim_state_update.m
+++ matlab_sim_src/sim_state_update.m
@@ -1,8 +1,8 @@
-function dy = sim_state_update(t,y,g)
+function dy = sim_state_update(t,y,ddq_fn)
 
 torque = 20*y(3) + 0.80 * y(4) % Arbitrary proportional controller
 
-ddq = g(y(3), y(4), torque); %phi, dphi, torque
+ddq = ddq_fn(y(3), y(4), torque); %phi, dphi, torque
 
 dy = [y(2);
       ddq(1);
