e295e5a481591d914d1e5faee8d2243cfe6eb52e
= <=>
2019-11-18 21:04:51 -0500

    Added calibration script, checking to see if our IMU is pre calibrated in setup

--- arduino_src/prototype/prototype.ino
+++ arduino_src/prototype/prototype.ino
@@ -26,6 +26,8 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 //Encoder E2(20, 21);
 
 PIDController pid_pitch;
+PIDController pid_wheel;
+
 void setup(void) 
 {
   // Initialize PWM pins as outputs
@@ -38,6 +40,9 @@ void setup(void)
   
   delay(100);
   Serial.println("Orientation Sensor Test"); Serial.println("");
+
+
+  
   /* Initialise the sensor */
   if(!bno.begin())
   {
@@ -45,6 +50,35 @@ void setup(void)
     Serial.println("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
+
+  uint8_t sys, gyro, accel, mag;
+  sys = gyro = accel = mag = 0;
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+  Serial.print("System calibration level: ");
+  Serial.println(sys);
+  Serial.print("Gyro calibration level: ");
+  Serial.println(gyro);
+  Serial.print("Accel calibration level: ");
+  Serial.println(accel);
+  Serial.print("Mag calibration level: ");
+  Serial.println(mag);
+
+  Serial.println("Setting calibration...");
+  adafruit_bno055_offsets_t calib;
+  calib.accel_offset_x = 0;
+  calib.accel_offset_y = 0;
+  calib.accel_offset_z = 0;
+  calib.mag_offset_x = 0;
+  calib.mag_offset_y = 0;
+  calib.mag_offset_z = 0;
+  calib.gyro_offset_x = 0;
+  calib.gyro_offset_y = 0;
+  calib.gyro_offset_z = 0;
+  calib.accel_radius = 0;
+  calib.mag_radius = 0;
+  //bno.setSensorOffsets(calib);
+  Serial.println("Didn't actually set calibration");
+
   delay(1000);
     
   bno.setExtCrystalUse(true);
@@ -53,6 +87,11 @@ void setup(void)
   pid_pitch.setpoint(0);    // The "goal" the PID controller tries to "reach"
   pid_pitch.tune(50, 0, 0);    // Tune the PID, arguments: kP, kI, kD
   pid_pitch.limit(-255, 255);    // limit to -255->255
+
+  pid_wheel.begin();          // initialize the PID instance
+  pid_wheel.setpoint(0);    // The "goal" the PID controller tries to "reach"
+  pid_wheel.tune(0, 0, 0);    // Tune the PID, arguments: kP, kI, kD
+  pid_wheel.limit(-30, 30);    // limit to -255->255
   
   Serial.println("Exiting setup");
     
@@ -72,11 +111,14 @@ void loop(void)
   //long enc1 = -E1.read();
             
   // Read IMU
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  sensors_event_t orientationData , angVelocityData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  //bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  
+  //imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  double pitch = orientationData.orientation.x;
 
-  double pitch = euler.z();
-
-  int control_sig = pid_pitch.compute(pitch);
+  int control_sig = pid_pitch.compute(pitch) + pid_wheel.compute(pitch);
   if (control_sig < 0)
   {
     digitalWrite(dir_1, HIGH);
@@ -88,7 +130,6 @@ void loop(void)
     digitalWrite(dir_2, LOW);
   }
   
-  //int mag = max(min(abs(control_sig), 255), 0);
   int mag = abs(control_sig);
   analogWrite(pwm_1, mag);
   analogWrite(pwm_2, mag);
