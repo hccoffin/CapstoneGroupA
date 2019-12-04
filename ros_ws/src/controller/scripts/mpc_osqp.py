957e79537e738a1bda75133d3f8a367e8d3c2f0f
= <=>
2019-12-03 19:41:58 -0500

    QP solver working in python!i

--- ros_ws/src/controller/scripts/mpc_osqp.py
+++ ros_ws/src/controller/scripts/mpc_osqp.py
@@ -10,6 +10,7 @@ import std_msgs
 # Optimization/Control Imports
 import osqp
 import numpy as np
+from numpy.matlib import repmat
 import scipy as sp
 from scipy import sparse
 
@@ -18,22 +19,57 @@ import odrive
 import bios
 
 # Load A,B dynamics matrices, costs Q, R and LQR backup gains K from YAML
-params_filename = "../config/control_params.mat"
-control_params = sio.loadmat(params_filename)
+params_filename = "../config/control_params.yaml"
+control_params = bios.read(params_filename)
 
-Ad = control_params['Ad']
-Bd = control_params['Bd']
-Q = control_params['Q']
-R = control_params['R']
-dt = control_params['dt']
+Ad = sparse.csc_matrix(control_params['Ad'])
+Bd = sparse.csc_matrix(control_params['Bd'])
+Q = sparse.csc_matrix(control_params['Q'])
+R = sparse.csc_matrix(control_params['R'])
 Qn = 3*Q # Slightly higher weights on final state error
+N = int(control_params['N'])
+dt = control_params['dt']
 nx,nu = Bd.shape
-N = 250 # num states to look ahead
 
-print(control_params)
+# Setup objective (1/2*x'*P*x + q'*x)
+xr = np.zeros([nx, 1]) # reference x
+x0 = np.reshape(np.array([0.5,0,0,0,0,0]), [nx, 1]) # initial x
+P = sparse.block_diag([sparse.kron(sparse.eye(N), Q), Qn, sparse.kron(sparse.eye(N), R)], format='csc')
+
+q = np.vstack([repmat(np.dot(-Q.todense(), xr), N, 1), np.dot(-Qn.todense(), xr), np.zeros([N*nu,1])])
+
+# Setup equality constraints (dynamics)
+Adx1 = sparse.kron(sparse.eye(N), Ad)
+Adx2 = np.zeros([N*nx, nx])
+Adx = sparse.csc_matrix(sparse.hstack([Adx1, Adx2]))
+x_plus1 = np.zeros([N*nx,nx])
+x_plus2 = sparse.kron(sparse.eye(N), sparse.eye(nx))
+x_plus = sparse.csc_matrix(sparse.hstack([x_plus1, x_plus2]))
+Bdu = sparse.kron(sparse.eye(N), Bd)
+
+Aeq = sparse.hstack([Adx-x_plus, Bdu])
+leq = np.zeros([N*nx, 1])
+ueq = np.zeros([N*nx, 1])
+
+# Setup inequality constraints (initial conditions, bounds)
+xmin = np.reshape(np.array([-np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf]), [nx,1])
+xmax = np.reshape(np.array([np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]), [nx, 1])
+umin = np.reshape(np.array([-2, -2]), [nu, 1])
+umax = np.reshape(np.array([2, 2]), [nu, 1])
+
+Aineq = sparse.eye(nx+N*nx+N*nu) # N+1 constrains on x, N constraints on u
+lineq = np.vstack([x0, repmat(xmin, N, 1), repmat(umin, N, 1)])
+uineq = np.vstack([x0, repmat(xmax, N, 1), repmat(umax, N, 1)])
 
 
+# Stack eq and ineq constraints
+A = sparse.vstack([Aeq, Aineq], format='csc')
+l = np.vstack([leq, lineq])
+u = np.vstack([ueq, uineq])
 
+# Setup OSQP instance and solve
 prob = osqp.OSQP()
 
-prob.solve()
+prob.setup(P, q, A, l, u, warm_start=True)
+
+res = prob.solve()
